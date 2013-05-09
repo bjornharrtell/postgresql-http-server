4651c2b8011734aace1fe7fa35cf884b93326d4c
Björn Harrtell <bjorn@wololo.org>
2013-05-09 22:10:35 +0200

    Create PostGIS extension

--- test/sql/init.sql
+++ test/sql/init.sql
@@ -1,3 +1,4 @@
+CREATE EXTENSION postgis;
 DROP SCHEMA IF EXISTS testschema CASCADE;
 CREATE SCHEMA testschema;
 CREATE TABLE testschema.testtable (id SERIAL PRIMARY KEY, name varchar, geom GEOMETRY(Point));
