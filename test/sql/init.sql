88de9dcfb578c0008fcb1e66e32c43a73ec59321
Björn Harrtell <bjorn@wololo.org>
2013-05-09 22:58:05 +0200

    Try to fix Travis PostGIS install

--- test/sql/init.sql
+++ test/sql/init.sql
@@ -2,4 +2,4 @@ CREATE EXTENSION postgis;
 DROP SCHEMA IF EXISTS testschema CASCADE;
 CREATE SCHEMA testschema;
 CREATE TABLE testschema.testtable (id SERIAL PRIMARY KEY, name varchar);
-SELECT AddGeometryColumn('testschema', 'testtable', 'geom', 0, 'POINT', 2);
+SELECT AddGeometryColumn('testschema', 'testtable', 'geom', -1, 'POINT', 2);
